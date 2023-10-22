5f741083caa3d3490b8fd53d8e1bae181754cb04
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-10-22 10:10:43 +0530

    first commit

---
+++ database/conn.js
@@ -0,0 +1,22 @@
+// import mongoose from "mongoose";
+
+// export default async function connect(){
+//     await mongoose.connect(process.env.ATLAS_URI)
+//     console.log(err);
+//     //await mongoose.connect("mongodb+srv://adityavishal903:aditya8076@cluster0.rxze5ev.mongodb.net/")
+//     console.log("Database Connected")
+// }
+
+import mongoose from "mongoose";
+
+export default async function connect() {
+    try {
+        await mongoose.connect(process.env.ATLAS_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database Connected");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+    }
+}
